9c1aee0512380d1a88da443dcd6c9daef2712777
Prof <profcoder1072005@gmail.com>
2026-03-13 19:54:08 +0200

    Resolve conflict in User.cs

--- Entities/User.cs
+++ Entities/User.cs
@@ -6,5 +6,14 @@ namespace EF_LSM.Entities
 {
     internal class User
     {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public UserRole Role { get; set; }
     }
 }
